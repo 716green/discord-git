fe913c180a8266a9d616f7541563df92dec31e4c
Bob Bass <bob@bbass.co>
2022-05-31 16:53:24 -0700

    created new endpoint

--- src/index.js
+++ src/index.js
@@ -6,4 +6,12 @@ app.get("/", (_req, res) => {
   res.json({ hello: "world!" });
 });
 
+app.get("/person", (_req, res) => {
+  res.json({
+    name: "Bob",
+    age: 32,
+    employed: false,
+  });
+});
+
 app.listen(port, () => console.log(`listening at http://localhost:${port}`));
